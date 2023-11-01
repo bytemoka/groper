beab7787de83bf295c0968bcf27d67af2582b0c1
宰容 <jaeyong.sinn@gmail.com>
2023-11-01 16:10:38 +0900

    move

--- animation-sample/animation.js
+++
@@ -1,7 +0,0 @@
-var anmation = bodymovin.loadAnimation({
-  container : document.querySelector('.animation'),
-  renderer : 'svg',
-  loop : true,
-  autoplay : true,
-  path: 'https://assets8.lottiefiles.com/packages/lf20_b32f3vtz.json' // 2-1. url
-});
