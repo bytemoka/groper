ca4ccee0ce5b9d49928f63aecd4690462201879d
宰容 <jaeyong.sinn@gmail.com>
2023-11-07 02:09:17 +0900

    add lottie

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+var anmation = bodymovin.loadAnimation({
+  container : document.querySelector('.animation'),
+  renderer : 'svg',
+  loop : true,
+  autoplay : true,
+  path: 'https://lottie.host/2e0e1b20-9527-4919-889a-7da9484cdc22/ogw3YUbY4X.json' // 2-1. url
+});
